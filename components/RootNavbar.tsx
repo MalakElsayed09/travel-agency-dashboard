7943248f3f60118aa6fb89ed03b16be1854a5c3d
Malak Elsayed <elsayedmalak93@gmail.com>
2026-01-09 10:28:57 -0500

    Moved assets

--- components/RootNavbar.tsx
+++ components/RootNavbar.tsx
@@ -9,6 +9,14 @@ import {
 import { logoutUser } from "~/appwrite/auth";
 import { cn } from "~/lib/utils";
 
+// Temporary inline SVG icons - these will work immediately
+const logoSvg =
+  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white'%3E%3Cpath d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'/%3E%3C/svg%3E";
+const logoutSvg =
+  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white'%3E%3Cpath d='M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z'/%3E%3C/svg%3E";
+const defaultAvatar =
+  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23666'%3E%3Cpath d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z'/%3E%3C/svg%3E";
+
 const RootNavbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -31,11 +39,7 @@ const RootNavbar = () => {
     >
       <header className="root-nav wrapper">
         <Link to="/" className="link-logo">
-          <img
-            src="/assets/icons/logo.svg"
-            alt="logo"
-            className="size-[30px]"
-          />
+          <img src={logoSvg} alt="logo" className="size-[30px]" />
           <h1>Tourvisto</h1>
         </Link>
 
@@ -52,21 +56,18 @@ const RootNavbar = () => {
           )}
 
           <img
-            src={user?.imageUrl || "/assets/images/david.wepb"}
+            src={user?.imageUrl || defaultAvatar}
             alt="user"
             referrerPolicy="no-referrer"
           />
 
           <button onClick={handleLogout} className="cursor-pointer">
-            <img
-              src="/assets/icons/logout.svg"
-              alt="logout"
-              className="size-6 rotate-180"
-            />
+            <img src={logoutSvg} alt="logout" className="size-6 rotate-180" />
           </button>
         </aside>
       </header>
     </nav>
   );
 };
+
 export default RootNavbar;
